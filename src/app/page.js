44e62e73a83eb3b4b4205c4fa5af66906e35e8a8
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-05-16 12:31:23 +0200

    hero section for home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
 
 export default function Home() {
   return (
-    <Header></Header>
+      <>
+
+    <Header/>
+     <Hero/>
+      </>
   );
 }
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Right from "@/components/icons/Right";
+
+export default function Hero() {
+    return (
+        <section className={"grid grid-cols-2"}>
+            <div>
+
+
+                <h1 className={"text-4xl font-semibold"}>
+                    Pizza is better than your Girlfriend
+                </h1>
+
+                <p className={"text-gray-500 my-4"}>Enjoy pizza and have fun</p>
+
+                <div className={"flex gap-4"}>
+                    <button className={"bg-primary flex gap-2  text-white px-4 py-2 rounded-full"}>Order now
+                    <Right/>
+                    </button>
+                    <button className={"flex gap-2 text-gray-500 py-2 font-semibold"}>Learn now
+                    <Right/>
+                    </button>
+                </div>
+            </div>
+
+            <div className={"relative"}>
+                <Image src={"/pizza.png"} alt={"pizza"} layout={'fill'}
+                objectFit={"contain"}></Image>
+            </div>
+
+
+
+        </section>
+    )
+}
