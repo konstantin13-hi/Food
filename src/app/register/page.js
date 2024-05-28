2e7c24ad8a2c8faf52910bef46d5395b7ed33a12
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-05-29 01:17:06 +0200

    added auth

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,18 +1,38 @@
+"use client"
+import {useState} from "react";
+
+
+
+
 export default function RegisterPage(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(ev){
+        ev.preventDefault();
+        fetch('/api/register',
+            {method: 'POST',
+                body:JSON.stringify({email, password}),
+            headers:{'Content-type':'application/json'}})
+
+    }
     return(
        <section className={"mt-8"}>
            <h1 className={"text-center text-primary text-4xl"}
                >
                Register Page
            </h1>
-           <for className={"block max-w-xs mx-auto"}>
-               <input type="text" placeholder={"email"}/>
-               <input type="password" placeholder={"password"}/>
+           <form className={"block max-w-xs mx-auto"} onSubmit={handleSubmit}>
+               <input type="text" placeholder={"email"} value={email}
+                      onChange={e=>setEmail(e.target.value)}/>
+               <input type="password" placeholder={"password"} value={password}
+                      onChange={e=>setPassword(e.target.value)}/>
                <button type="submit">Register</button>
                <button>Login with google</button>
                <div className={"my-4 text-center text-gray-500"}>Login with provider</div>
 
-           </for>
+           </form>
+           <button >sdsds</button>
 
 
        </section>
