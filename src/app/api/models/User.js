44d0c0999369bada17c844a6af598265832f3168
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-05-29 16:24:20 +0200

    added mongo db for user

---
+++ src/app/api/models/User.js
@@ -0,0 +1,10 @@
+import mongoose, {model,models} from "mongoose";
+
+const UserSchema
+    = new mongoose.Schema({
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+},{ timestamps: true })
+
+
+export const User =models?.User || model('User', UserSchema)
--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,4 +1,11 @@
-export function POST(req) {
-    return Response.json("ok");
+import mongoose from "mongoose";
+import {User} from '../models/User'
+
+export async function POST(req) {
+    const body = await req.json();
+    await mongoose.connect("mongodb+srv://pizza:a65mSRte020sprAN@cluster0.cnoatez.mongodb.net/")
+    const createUser = await User.create(body);
+
+    return Response.json(createUser);
 
 }
