4906a12fff619c5bee07c4e4145fd5e7a52a2849
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-05-29 17:54:22 +0200

    fixed url security

--- src/app/api/models/User.js
+++ src/app/api/models/User.js
@@ -1,10 +1,24 @@
-import mongoose, {model,models} from "mongoose";
+import mongoose, {model, models} from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema
     = new mongoose.Schema({
     email: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
-},{ timestamps: true })
+    password: {
+        type: String, required: true, validate: pass => {
+            if (pass.length < 5) {
+                return Error("Password must be at least 5 characters")
+            }
+        }
+    },
+
+}, {timestamps: true})
+
+UserSchema.post("validate", function (user) {
+    const pass = user.password;
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(pass, salt);
+})
 
 
-export const User =models?.User || model('User', UserSchema)
+export const User = models?.User || model('User', UserSchema)
--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -3,7 +3,7 @@ import {User} from '../models/User'
 
 export async function POST(req) {
     const body = await req.json();
-    await mongoose.connect("mongodb+srv://pizza:a65mSRte020sprAN@cluster0.cnoatez.mongodb.net/")
+    await mongoose.connect(process.env.MONGODB_URI)
     const createUser = await User.create(body);
 
     return Response.json(createUser);
