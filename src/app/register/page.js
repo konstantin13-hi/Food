8f2ba888ab0eecd497c6009af28ed81d8b3c5e20
konstantin13-hi <konstantinchizhevskiy@gmail.com>
2024-05-29 22:38:30 +0200

    finished register page with email

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,7 @@
 "use client"
 import {useState} from "react";
+import Link from "next/link";
+import {error} from "next/dist/build/output/log";
 
 
 
@@ -7,13 +9,31 @@ import {useState} from "react";
 export default function RegisterPage(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [creatingUser, setCreatingUser] = useState(false);
+    const [userCreated, setUserCreated] = useState(true);
+    const [error, setError] = useState(false);
 
-    function handleSubmit(ev){
-        ev.preventDefault();
-        fetch('/api/register',
-            {method: 'POST',
-                body:JSON.stringify({email, password}),
-            headers:{'Content-type':'application/json'}})
+   async function handleSubmit(ev){
+       ev.preventDefault();
+       setCreatingUser(true)
+       setError(false)
+       setUserCreated(false)
+
+      const response = await fetch('/api/register',
+               {
+                   method: 'POST',
+                   body: JSON.stringify({email, password}),
+                   headers: {'Content-type': 'application/json'}
+               })
+
+
+       if(response.ok){
+           setUserCreated(true);
+       }
+       else {
+           setError(true);
+       }
+       setCreatingUser(false);
 
     }
     return(
@@ -22,17 +42,34 @@ export default function RegisterPage(){
                >
                Register Page
            </h1>
+           {userCreated && (
+               <div className="flex justify-center">
+                   User Created &nbsp;
+                   <Link className={"underline"} href="/login"> Log in </Link>
+               </div>
+           )}
+           {error && (
+               <div className="flex justify-center">
+                   Error &nbsp;
+
+               </div>
+           )}
            <form className={"block max-w-xs mx-auto"} onSubmit={handleSubmit}>
                <input type="text" placeholder={"email"} value={email}
+                      disabled={creatingUser}
                       onChange={e=>setEmail(e.target.value)}/>
                <input type="password" placeholder={"password"} value={password}
+                      disabled={creatingUser}
                       onChange={e=>setPassword(e.target.value)}/>
                <button type="submit">Register</button>
                <button>Login with google</button>
                <div className={"my-4 text-center text-gray-500"}>Login with provider</div>
 
            </form>
-           <button >sdsds</button>
+           <div className="flex justify-center">
+               Existing account? &nbsp; <Link className={"underline"} href="/login">Log in</Link>
+           </div>
+
 
 
        </section>
